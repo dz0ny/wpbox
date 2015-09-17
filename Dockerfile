2a2b739f15c8e765e1e299cc2a5e32395a92e7a4
dz0ny <dz0ny@ubuntu.si>
2015-09-17 16:01:05 +0200

    Add xdebug support

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,12 @@
-FROM wordpress:4.1
-RUN apt-get update 
+FROM wordpress:4.3-apache
+RUN apt-get update
 RUN apt-get install unzip wget -y
 RUN mkdir -p /var/www/html/wp-content/mu-plugins
+RUN curl "http://xdebug.org/files/xdebug-2.3.3.tgz" -o /tmp/xdebug.tar.gz
+RUN tar -xvf /tmp/xdebug.tar.gz -C /tmp
+RUN cd /tmp/xdebug-2.3.3/ && phpize
+RUN cd /tmp/xdebug-2.3.3/ && ./configure --enable-xdebug --with-php-config=/usr/local/bin/php-config
+RUN cd /tmp/xdebug-2.3.3/ && make install
+RUN echo 'zend_extension="/usr/local/lib/php/extensions/no-debug-non-zts-20131226/xdebug.so"' > /usr/local/etc/php/conf.d/xdebug.ini
 COPY mu.php /var/www/html/wp-content/mu-plugins/index.php
 COPY run.sh /entrypoint.sh
--- mu.php
+++ mu.php
@@ -1,57 +1,71 @@
 <?php
-    add_filter( 'filesystem_method', create_function( '$a', 'return "direct";' ) );
 
-    function _default_uploads_use_yearmonth_folders() {
+
+    add_filter('filesystem_method', create_function('$a', 'return "direct";'));
+
+    function _default_uploads_use_yearmonth_folders()
+    {
         return '';
     }
 
-    add_filter( 'pre_option_uploads_use_yearmonth_folders', "_default_uploads_use_yearmonth_folders" );
+    add_filter('pre_option_uploads_use_yearmonth_folders', '_default_uploads_use_yearmonth_folders');
 
-    function install_defaults( $user ) {
+    function install_defaults($user)
+    {
         global $wp_rewrite;
-        $wp_rewrite->set_permalink_structure("/%postname%/");
+        $wp_rewrite->set_permalink_structure('/%postname%/');
         flush_rewrite_rules();
         auto_login();
     }
 
-    function auto_login() {
-        if (!is_user_logged_in() ) {
-            $users = get_users( array( 'role' => 'administrator' ) );
-            if ( count( $users ) > 0 ) {
+    function auto_login()
+    {
+        if (!is_user_logged_in()) {
+            $users = get_users(array('role' => 'administrator'));
+            if (count($users) > 0) {
                 $user = $users[0];
-                wp_set_auth_cookie( $user->ID, true, '' );
-                do_action( 'wp_login', $user->get( 'user_login' ) );
+                wp_set_auth_cookie($user->ID, true, '');
+                do_action('wp_login', $user->get('user_login'));
             }
         }
     }
-    function add_toolbar_items($admin_bar){
-        $admin_bar->add_menu( array(
-            'title' => 'Reset',
-            'href'  => '/wp-admin/reset.php',
-            'meta'  => array(
-                'title' => "Reset",            
-            ),
-        ));
+
+    function dynamic_rewrite()
+    {
+        return 'http://'.$_SERVER['HTTP_HOST'];
     }
 
-    function dynamic_rewrite( ) {
-       return "http://".$_SERVER['HTTP_HOST'];
+    function env_render()
+    {
+        ?>
+        <h2>Tools</h2>
+        <a href="http://<?php echo $_ENV['MAILHOG_PORT_1025_TCP_ADDR']; ?>:8025">Mail inbox</a>
+        <h2>Enviroment</h2>
+        <pre><?php var_dump($_ENV); ?></pre>
+        <h2>PHPInfo</h2>
+        <pre><?php phpinfo(); ?></pre>
+        <?php
+
     }
 
-    add_action( 'wp_install', 'install_defaults', 9999, 1 );
-    add_filter ( 'pre_option_home', 'dynamic_rewrite' );
-    add_filter ( 'pre_option_siteurl', 'dynamic_rewrite' );
+    function admin_menu_mu()
+    {
+        add_dashboard_page('Enviroment', 'Enviroment', 'read', 'env', 'env_render');
+    }
 
-    if (!is_blog_installed() && !defined('WP_INSTALLING')) {
-        require_once( ABSPATH . WPINC . '/pluggable.php' );
-        $link = wp_guess_url() . '/wp-admin/install.php';
-        $args = array(
-            'headers' => array( 'Content-Type' => 'application/x-www-form-urlencoded' ),
-            'body' => 'weblog_title=admin&user_name=admin&admin_password=admin&admin_password2=admin&admin_email=admin%40wordpress.org&blog_public=1&language=',
-        );
-         wp_remote_post( $link, $args );
+    function phpmailer_init_mailhog(&$phpmailer)
+    {
+        $phpmailer->Mailer = 'smtp';
+        $phpmailer->SMTPSecure = 'none';
+        $phpmailer->Host = $_ENV['MAILHOG_PORT_1025_TCP_ADDR'];
+        $phpmailer->Port = 1025;
+        $phpmailer->SMTPAuth = false;
+    }
 
-    }else{
-        add_action( 'wp_loaded', 'auto_login', 9999);
-        add_action('admin_bar_menu', 'add_toolbar_items', 100);
+    add_action('wp_install', 'install_defaults', 9999, 1);
+    add_filter('pre_option_home', 'dynamic_rewrite');
+    add_filter('pre_option_siteurl', 'dynamic_rewrite');
+    add_action('phpmailer_init', 'phpmailer_init_mailhog');
+    if (is_blog_installed()) {
+        add_action('admin_menu', 'admin_menu_mu');
     }
